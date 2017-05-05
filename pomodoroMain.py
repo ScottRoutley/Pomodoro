6235444c9c99b7f1ff9b455727a63b13a4fac87c
ScottRoutley <scott.routley@gmail.com>
2017-05-04 23:23:52 -0500

    put in some random stuff related to button pressing

--- pomodoroMain.py
+++ pomodoroMain.py
@@ -1,7 +1,7 @@
 import time
 
 from Adafruit_LED_Backpack import SevenSegment
-
+import RPi.GPIO as GPIO  
 
 # Create display instance on default I2C address (0x70) and bus number.
 display = SevenSegment.SevenSegment()
@@ -17,6 +17,19 @@ colon = True
 
 twentyFiveMinutes = 1500
 
+GPIO.setmode(GPIO.BCM)  
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+
+def buttonPress(channel):  
+    print "Button pressed!"
+
+GPIO.add_event_detect(24, GPIO.RISING, callback=buttonPress, bouncetime=300)
+
+raw_input("Listening...")
+        # clean up GPIO on normal exit  
+
+
+ 
 
 # Run through different number printing examples.
 print('Press Ctrl-C to quit.')
@@ -43,3 +56,5 @@ while True:
         time.sleep(1.0)
         twentyFiveMinutes -=1
     # Print the same numbers with 1 digit precision.
+
+GPIO.cleanup()   
