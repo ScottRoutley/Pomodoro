f1fb05e73117fff71cc83fa69431b548c9054be6
ScottRoutley <scott.routley@gmail.com>
2017-05-04 22:59:18 -0500

    first time commit

--- pomodoroMain.py
+++ pomodoroMain.py
@@ -15,16 +15,22 @@ display.begin()
 # Keep track of the colon being turned on or off.
 colon = True
 
+twentyFiveMinutes = 1500
+
+
 # Run through different number printing examples.
 print('Press Ctrl-C to quit.')
 numbers = [0.0, 1.0, -1.0, 0.55, -0.55, 10.23, -10.2, 100.5, -100.5]
 while True:
     # Print floating point values with default 2 digit precision.
     for i in numbers:
+        m, s = divmod(twentyFiveMinutes, 60)
+
+        currentTime = m + (s/100)
         # Clear the display buffer.
         display.clear()
         # Print a floating point number to the display.
-        display.print_float(i)
+        display.print_float(currentTime)
         # Set the colon on or off (True/False).
         display.set_colon(colon)
         # Write the display buffer to the hardware.  This must be called to
@@ -32,4 +38,5 @@ while True:
         display.write_display()
         # Delay for a second.
         time.sleep(1.0)
+        twentyFiveMinutes -=1
     # Print the same numbers with 1 digit precision.
