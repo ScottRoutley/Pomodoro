ab8d158720b5093b7c6ed05b56c72269ce7f8dae
ScottRoutley <scott.routley@gmail.com>
2017-05-04 22:47:53 -0500

    first commit

---
+++ pomodoroMain.py
@@ -0,0 +1,35 @@
+import time
+
+from Adafruit_LED_Backpack import SevenSegment
+
+
+# Create display instance on default I2C address (0x70) and bus number.
+display = SevenSegment.SevenSegment()
+
+# Alternatively, create a display with a specific I2C address and/or bus.
+# display = SevenSegment.SevenSegment(address=0x74, busnum=1)
+
+# Initialize the display. Must be called once before using the display.
+display.begin()
+
+# Keep track of the colon being turned on or off.
+colon = False
+
+# Run through different number printing examples.
+print('Press Ctrl-C to quit.')
+numbers = [0.0, 1.0, -1.0, 0.55, -0.55, 10.23, -10.2, 100.5, -100.5]
+while True:
+    # Print floating point values with default 2 digit precision.
+    for i in numbers:
+        # Clear the display buffer.
+        display.clear()
+        # Print a floating point number to the display.
+        display.print_float(i)
+        # Set the colon on or off (True/False).
+        display.set_colon(colon)
+        # Write the display buffer to the hardware.  This must be called to
+        # update the actual display LEDs.
+        display.write_display()
+        # Delay for a second.
+        time.sleep(1.0)
+    # Print the same numbers with 1 digit precision.
